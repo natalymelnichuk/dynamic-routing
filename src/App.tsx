772916575a539926ab6727f98c975490b58f9cc3
Natallia <ntshmlnchuk@gmail.com>
2026-09-15 13:11:04 -0400

    BlogPost completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
+import BlogIndex from './pages/BlogIndex'
+import { PostPage } from './pages/BlogPost'
 
 function App() {
   
-
   return (
-    <>
-      <h1>Dynamic routing</h1>
-    </>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path='/' element={<Navigate replace to="/blog"/>} />
+          <Route path='/blog' element={<BlogIndex />} />
+          <Route path='/blog/:slug' element={<PostPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
--- src/pages/BlogPost.tsx
+++ src/pages/BlogPost.tsx
@@ -1,1 +1,29 @@
 
+import { useParams } from "react-router-dom";
+import { posts } from "../lib/posts";
+
+export const PostPage = () => {
+    const { slug } = useParams();
+
+    const post = posts.find((p) => p.slug === slug);
+
+    if (!post) {
+        return (
+        <div className="max-w-2xl mx-auto p-4 text-red-500">
+            <h2 className="text-2xl font-bold text-red-600 mb-2">Post not found</h2>
+        </div>
+        );
+    }
+
+    return (
+        <article className="max-w-2xl mx-auto p-6">
+            <h1 className="text-3xl font-extrabold mb-4 text-gray-900">{post.title}</h1>
+            <div className="prose text-gray-700 leading-relaxed whitespace-pre-line">
+                {post.content}
+            </div>
+        </article>
+    )
+};
+
+
+
