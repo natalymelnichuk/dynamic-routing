b06ed5e02f09e685d5590c1e3bb17c1f9fa2ecbe
Natallia <ntshmlnchuk@gmail.com>
2026-09-15 11:28:02 -0400

    Posts file completed + main files were added

---
+++ src/lib/posts.ts
@@ -0,0 +1,40 @@
+
+export interface Post {
+    id: string | number;
+    slug: string;
+    title: string;
+    content: string;
+}
+
+export const posts = [
+        {
+        id: 1,
+        slug: "sunt-aut-facere",
+        title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+        content: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    },
+    {
+        id: 2,
+        slug: "qui-est-esse",
+        title: "qui est esse",
+        content: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+    },
+    {
+        id: 3,
+        slug: "ea-molestias-quasi",
+        title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+        content: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+    },
+    {        
+        id: 4,
+        slug: "eum-et-est-occaecati",
+        title: "eum et est occaecati",
+        content: "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
+    },
+    {
+        id: 5,
+        slug: "nesciunt-quas-odio",
+        title: "nesciunt quas odio",
+        content: "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
+    },
+]
