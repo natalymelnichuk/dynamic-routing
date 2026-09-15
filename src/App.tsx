78da3bae9692501cafb453a1ca9310a51c23b6f4
Natallia <ntshmlnchuk@gmail.com>
2026-09-15 13:54:46 -0400

    Animation was added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom'
 import './App.css'
+import { AnimatePresence } from 'framer-motion'
 import BlogIndex from './pages/BlogIndex'
 import { PostPage } from './pages/BlogPost'
 import Login from './pages/Login'
@@ -7,33 +8,55 @@ import Navbar from './components/Navbar'
 import { AuthProvider } from './context/AuthContext'
 import ProtectedRoute from './components/ProtectedRoute'
 import AdminDashboard from './pages/AdminDashboard'
+import AnimatedPage from './components/AnimatedPage'
 
 
-function App() {  
+
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Navigate replace to="/blog" />} />
+        <Route 
+          path="/blog" 
+          element={<AnimatedPage><BlogIndex /></AnimatedPage>} 
+        />
+        <Route 
+          path="/blog/:slug" 
+          element={<AnimatedPage><PostPage /></AnimatedPage>} 
+        />
+        <Route 
+          path="/login" 
+          element={<AnimatedPage><Login /></AnimatedPage>} 
+        />
+        <Route 
+          path="/admin" 
+          element={
+            <ProtectedRoute>
+              <AnimatedPage><AdminDashboard /></AnimatedPage>
+            </ProtectedRoute>
+          } 
+        />
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
+function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <div className="min-h-screen bg-gray-50 text-gray-900">
           <Navbar />
-
           <main>
-            <Routes>
-              <Route path='/' element={<Navigate replace to="/blog"/>} />
-              <Route path='/blog' element={<BlogIndex />} />
-              <Route path='/blog/:slug' element={<PostPage />} />
-              <Route path='/login' element={<Login />} />
-
-              <Route path='/admin' element={
-                <ProtectedRoute>
-                  <AdminDashboard />
-                </ProtectedRoute>
-              } />
-            </Routes>
+            <AnimatedRoutes />
           </main>
         </div>
       </BrowserRouter>
     </AuthProvider>
-  )
+  );
 }
 
 export default App;
